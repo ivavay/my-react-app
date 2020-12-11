cd47ad562fa0fe80fe6200c64f25944a40f381d3
Ivy Chen <ichen9@ucsc.edu>
2020-12-11 20:30:49 +0800

    added stylings

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,13 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>A multiciplinary designer based in Taipei, Taiwan</h1>
-    <p>Always experimenting + making</p>
-    <Link to="/page-2/">More</Link>
+    <div className = "Hero">
+      <div className = "HeroGroup">
+      <h1>A multiciplinary designer based in Taipei, Taiwan</h1>
+      <p>Always experimenting + making</p>
+      <Link to="/page-2/">More</Link>
+      </div>
+    </div>
   </div>
 )
 
