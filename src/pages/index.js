deb8f77d8ee59c52c5404c9836b11900a9f01ae2
Ivy Chen <ichen9@ucsc.edu>
2020-12-12 15:36:04 +0800

    added animation to landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ const IndexPage = () => (
     <div className = "Hero">
       <div className = "HeroGroup">
       <h1>A multiciplinary designer based in Taipei, Taiwan</h1>
-      <p>Always experimenting + making</p>
+      <p>Interested in the area of non-traditional learnings and how that might enable more people to pursue their creative interests. In my free time, I love to spend time exploring the space between reality and art through honing my skills in 3D and creative coding. </p>
       <Link to="/page-2/">More</Link>
       </div>
     </div>
