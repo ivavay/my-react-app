bb901541ffcc8ec1791b160ff7a2a47963946e08
Ivy Chen <ichen9@ucsc.edu>
2020-12-16 20:39:19 +0800

    made changes to wave

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ const IndexPage = () => (
       <Link to="/page-2/">See work</Link>
       <svg width="2560" height="172" viewBox="0 0 100% 172" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path fill="#4C4DEF">
-     <animate repeatCount =  "indefinite" fill = "freeze" attributeName = "d" dur = "10s" values = "
+     <animate repeatCount =  "indefinite" fill = "freeze" attributeName = "d" dur = "10s" values ="
      
             M0 25.9086C277 84.5821 433 65.736 720 25.9086C934.818 -3.9019 1214.06 -5.23669 1442 8.06597C2079 45.2421 2208 63.5007 2560 25.9088V171.91L0 171.91V25.9086Z;
             
